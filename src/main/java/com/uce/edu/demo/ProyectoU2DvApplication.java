c72be41c274fc6ec06a476926210dd95f6ceb67b
Domenica Vizcarra <domeviz3410@gmail.com>
2022-06-27 16:37:36 -0500

    Tarea 12: Configuracion Log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2DvApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2DvApplication.java
@@ -1,13 +1,26 @@
 package com.uce.edu.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.Logger;
 
 @SpringBootApplication
-public class ProyectoU2DvApplication {
+public class ProyectoU2DvApplication implements CommandLineRunner {
 
+	private static Logger LOG=Logger.getLogger(ProyectoU2DvApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2DvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+		
+		LOG.info("Tarea 12: Configuración Log4j");
 	}
 
 }
