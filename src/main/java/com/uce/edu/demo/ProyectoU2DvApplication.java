4db080ab73a0c12a921015f57ddee3003053c305
Domenica Vizcarra <domeviz3410@gmail.com>
2022-07-19 20:10:38 -0500

    Taller 24: Mapeo JPA OneToOne

--- src/main/java/com/uce/edu/demo/ProyectoU2DvApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2DvApplication.java
@@ -1,6 +1,6 @@
 package com.uce.edu.demo;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,23 +8,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.estudiante.modelo.EstudianteContadorEdad;
-import com.uce.edu.demo.estudiante.modelo.EstudianteSencillo;
 import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
-import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
-import com.uce.edu.demo.repository.modelo.PersonaSencilla;
-import com.uce.edu.demo.service.IPersonaJpaService;
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
+import com.uce.edu.demo.service.ICiudadanoService;
 
 @SpringBootApplication
 public class ProyectoU2DvApplication implements CommandLineRunner {
 
 	private static Logger LOG = Logger.getLogger(ProyectoU2DvApplication.class);
-
-	@Autowired
-	private IPersonaJpaService iPersonaJpaService;
 	
 	@Autowired
 	private IEstudianteJpaService iEstudianteJpaService;
+	
+	@Autowired
+	private ICiudadanoService iCiudadanoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2DvApplication.class, args);
@@ -34,15 +32,31 @@ public class ProyectoU2DvApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		List<EstudianteSencillo> listaEstudiante=this.iEstudianteJpaService.buscarPorCursoSencillo("6");
-		for(EstudianteSencillo item: listaEstudiante) {
-			LOG.info(" Estudiantes del curso: " + item);
-		}
+		Ciudadano ciu1=new Ciudadano();
+		ciu1.setNombre("Domenica");
+		ciu1.setApellido("Vizcarra");
 		
-		List<EstudianteContadorEdad> mylistaEstudiante=this.iEstudianteJpaService.buscarCantidadPorEdad();
-		for(EstudianteContadorEdad item: mylistaEstudiante) {
-			LOG.info(" Edad: " + item);
-		}
+		Empleado empl1=new Empleado();
+		empl1.setCodigoIess("12121");
+		empl1.setSalario(new BigDecimal(100));
+		empl1.setCiudadano(ciu1);
+		
+		ciu1.setEmpleado(empl1);
+		
+		this.iCiudadanoService.guardarCiudadano(ciu1);
+		
+		Ciudadano ciu2=new Ciudadano();
+		ciu2.setNombre("Alison");
+		ciu2.setApellido("Vizcarra");
+		
+		Empleado empl2=new Empleado();
+		empl2.setCodigoIess("43434");
+		empl2.setSalario(new BigDecimal(500));
+		empl2.setCiudadano(ciu2);
+		
+		//Insertar empleado
+		//this.iCiudadanoService.guardarEmpleado(empl2);
+		
 	}
 
 }
